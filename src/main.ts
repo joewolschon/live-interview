46d9238d4756dc842dfd87a9a1ba2c6b76b895a0
Joseph Wolschon <jwolschon@payjunction.com>
2023-09-28 17:23:49 -0500

    Initial bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
+import {bootstrapApplication} from "@angular/platform-browser";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppModule, {providers: []})
+    .catch(err => console.error(err));
